5389040c7cb0bcb27f05f74b71a9a0e73cef58f3
sha345trip <114864315+sha345trip@users.noreply.github.com>
2024-05-20 15:01:43 +0530

    Cleaning

--- src/PostPage.js
+++ src/PostPage.js
@@ -1,11 +1,25 @@
-import { useParams, Link } from "react-router-dom";
-import { useContext } from "react";
-import DataContext from "./context/DataContext";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { useContext } from 'react';
+import api from './api/posts';
+import DataContext from './context/DataContext';
 
 const PostPage = () => {
-    const { posts, handleDelete } = useContext(DataContext);
+    const { posts, setPosts } = useContext(DataContext);
     const { id } = useParams();
+    const navigate = useNavigate();
     const post = posts.find(post => (post.id).toString() === id);
+
+    const handleDelete = async (id) => {
+        try {
+            await api.delete(`/posts/${id}`);
+            const postsList = posts.filter(post => post.id !== id);
+            setPosts(postsList);
+            navigate('/');
+        } catch (err) {
+            console.log(`Error: ${err.message}`);
+        }
+    }
+
     return (
         <main className="PostPage">
             <article className="post">
